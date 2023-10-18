2d9c727ebf319f67fa55356a244fc4f89f93ab57
g21turbo <dg21stew@gmail.com>
2023-10-17 22:58:18 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int highScorePosition = calculateHighScorePosition(3000);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tom", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(350);
+        displayHighScorePosition("Rick", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(99);
+        displayHighScorePosition("Steve", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String name, int scorePosition) {
+        System.out.println(name + " managed to get into position " + scorePosition +
+                            " on the high score list.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        }
+        return 4;
+
+    }
+}
